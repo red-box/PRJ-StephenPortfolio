9f4fb6d7387c289ab1d7daa1c7eedf6ba347f5ff
red-box <42344808+red-box@users.noreply.github.com>
2020-04-09 22:42:52 +0200

    Navigation Menu
    Logic
    
    Added Menu Toggle Logic.
    
    HTML
    Added all menu items.
    
    CSS
    Added Shadow for close buttons.

--- jquery.js
+++ jquery.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
+    /**Menu Toggle */
+    $('.burgerMenu').click(function(){
+        $('.menuWrapper').css('right','0%')
+    })
+    $('.menuClose').click(function(){
+        $('.menuWrapper').css('right','-100%')
+    })
+    /**Genre Button Toggle */
     $('.genreButton').click(function(){
         thisId = $(this).attr('id'); 
         $(`#${thisId}`).toggleClass(`${thisId}Active`)
     })
+
 })
